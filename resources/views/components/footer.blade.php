c5e9973f24feb43ca22d79c246ecb1ee7c9adc23
misawa-shota <kaiyou0120@gmail.com>
2023-07-24 23:38:00 +0900

    headerとfooterの作成

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="text-center footer_bg py-3">
+    <a class="navbar-brand fs-3" href="{{ url('/') }}">
+        {{ config('app.name', 'Nagoyameshi') }}
+    </a>
+    <div class="d-flex justify-content-center my-3">
+        <a href="#" class="me-4 text-decoration-none">
+            <span class="text-secondary">会社概要</span>
+        </a>
+        <a href="#" class="text-decoration-none">
+            <span class="text-secondary">利用規約</span>
+        </a>
+    </div>
+    <small>&copy;Nagoyameshi All rights reservrd.</small>
+</footer>
